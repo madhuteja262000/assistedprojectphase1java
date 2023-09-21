10c141b190a05b16657bdac325239015368cb611
teja <madhuteja26200@gmail.com>
2023-09-21 17:25:32 +0000

    done

---
+++ src/javaprogram/While.java
@@ -0,0 +1,17 @@
+package javaprogram;
+
+public class While {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 1;
+		while(i<=10) {
+		System.out.println(i);
+		i++;
+	}
+	System.out.println("Printed values form 1 to 10");
+
+
+	}
+
+}
